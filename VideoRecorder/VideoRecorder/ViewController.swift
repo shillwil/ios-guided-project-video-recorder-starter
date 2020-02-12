890e50f0e2f27da04c93924e2d1c851b500c8d1a
Alex Shillingford <alex.shillingford@icloud.com>
2020-02-12 10:44:18 -0700

    Set up permissions for camera and microphone usage

--- VideoRecorder/VideoRecorder/ViewController.swift
+++ VideoRecorder/VideoRecorder/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 	
@@ -20,9 +21,44 @@ class ViewController: UIViewController {
 		
 		// TODO: get permission
 		
-		showCamera()
+		requestPermissionAndShowCamera()
 		
 	}
+    
+    private func requestPermissionAndShowCamera() {
+        let status = AVCaptureDevice.authorizationStatus(for: .video)
+        
+        switch status {
+            
+        case .notDetermined: // User's first use of the app
+            requestVideoPermission()
+            
+        case .restricted: // Privacy (video is disabled, present UI to user)
+            fatalError("Video is disabled, please review parental controls")
+            
+        case .denied: // Asked for permission but they said no
+            fatalError("Tell the user they can't use the app without giving permission: Settings > Privacy > Video")
+            
+        case .authorized: // User previously gave permission, show the camera
+            showCamera()
+            
+        @unknown default:
+            fatalError("A new status code was added that we need to handle")
+        }
+    }
+    
+    private func requestVideoPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { (granted) in
+            guard granted else {
+                fatalError("Tell user to enable permission for Video/Camera")
+            }
+            
+            // Show camera
+            DispatchQueue.main.async {
+                self.showCamera()
+            }
+        }
+    }
 	
 	private func showCamera() {
 		performSegue(withIdentifier: "ShowCamera", sender: self)
